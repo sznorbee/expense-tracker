d7ad178520f7c6cba221be5f7765db4d7b626a57
Norbert Szekeres <norbert.szekeres@gmail.com>
2024-04-15 09:42:42 +0200

    Revert "lock device only portraitUp"
    This reverts commit 1e30a0604633eb4e99e05475a0e4de168672c758.

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
-import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-    seedColor:
-        const Color.fromARGB(255, 115, 29, 201) // #731DC9 to rgb = 115,29,201
-    );
+  seedColor: const Color.fromARGB(255, 115, 29, 201 ) // #731DC9 to rgb = 115,29,201
+);
 var kDarkColorScheme = ColorScheme.fromSeed(
-    brightness: Brightness.dark,
-    seedColor:
-        const Color.fromARGB(255, 5, 29, 201) // #731DC9 to rgb = 115,29,201
-    );
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 29, 201 ) // #731DC9 to rgb = 115,29,201
+);
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]).then((fn) => runApp(MaterialApp(
-        home: const Expenses(),
-        darkTheme: ThemeData.dark().copyWith(
-          colorScheme: kDarkColorScheme,
-          cardTheme: const CardTheme().copyWith(
-            color: kDarkColorScheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          ),
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: kDarkColorScheme.primaryContainer,
-              textStyle: const TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+  runApp(MaterialApp(
+    home: const Expenses(),
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      cardTheme: const CardTheme().copyWith(
+        color: kDarkColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          textStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
           ),
         ),
-        theme: ThemeData().copyWith(
-            colorScheme: kColorScheme,
-            appBarTheme: const AppBarTheme().copyWith(
-              backgroundColor: kColorScheme.onPrimaryContainer,
-              foregroundColor: kColorScheme.primaryContainer,
-            ),
-            cardTheme: const CardTheme().copyWith(
-              color: kColorScheme.secondaryContainer,
-              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            ),
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kColorScheme.primaryContainer,
-                textStyle: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            textTheme: ThemeData().textTheme.copyWith(
-                  titleLarge: const TextStyle().copyWith(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                )),
-      )));
+      ),
+    ),
+    theme: ThemeData().copyWith(
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      cardTheme: const CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer,
+          textStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      textTheme: ThemeData().textTheme.copyWith(
+        titleLarge: const TextStyle().copyWith(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+      )
+    ),
+  ));
 }
